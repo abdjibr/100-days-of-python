4276e3d14cf152ea41ca3dcd7d70562d74be50f2
abdjibr <abdjibr@gmail.com>
2025-06-30 19:11:36 +0200

    rename follder + start of ceasea chipher

---
+++ day01/print_statement.py
@@ -0,0 +1,12 @@
+#First task
+
+#print a statement, that says hello + my name and make the name as an input
+
+#adding line escape sequence to format strings in Python
+
+print("hello \n"+ input("whats your name \n") + "\n hello again")
+
+
+
+
+
---
+++ day08/Ceasar Cipher project.py
@@ -0,0 +1,25 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+#creating a def function and inside it a for loop the letter=each letter in the text.
+def encrypt (original_text, shift_amount):
+    #having a variable str where all the data from the for loop will be placed.
+    cipher_text = ""
+
+    for letter in original_text:
+        #starts with, finding the index nr. for the original letter
+        original_position = alphabet.index(letter)
+        #then creating a variable for the index nr. when the letter is shifted
+        shifted_position = original_position + shift_amount
+        #the final letter will be added to the cipher_text outside the for loop.
+        #cipher_text is  a str and the shifted_position is an int, it has to be the same operand
+        cipher_text += alphabet[shifted_position]
+        #finally i print the cipher_text outside the for loop.
+    print(cipher_text)
+
+#print the final results, and it works.
+print(encrypt(original_text=text,shift_amount=shift))
